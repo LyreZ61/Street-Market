9eb3d6202037906950f30803c8a7d5f60e64dd3e
LyreZ61 <mumix61@gmail.com>
2018-07-30 12:50:51 +0200

    test2

---
+++ Street Market/Assets/ButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+
+
+    public int MenschenAnzahl;
+    public GameObject go;
+
+
+    private void Awake()
+    {
+        
+    }
+
+    void Start () {
+        go.transform.position = new Vector3(1, 1, 1);
+	}
+	
+	void FixedUpdate () {
+		
+	}
+
+}
