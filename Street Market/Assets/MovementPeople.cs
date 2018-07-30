357a4d547220323b8d0ed88d83907b52cc4e5d3b
LyreZ61 <mumix61@gmail.com>
2018-07-30 16:23:10 +0200

    Movement Position

---
+++ Street Market/Assets/MovementPeople.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPeople : MonoBehaviour {
+
+    public float speed = 3f;
+
+    private Collider2D people;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (people == null)
+        {
+            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+
+        }
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        people = collision;
+    }
+}
